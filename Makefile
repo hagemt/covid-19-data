aa9bda390bedcdf36ac11636f4e006443d75d73f
Tor Hagemann <tor.hagemann@socotra.com>
2020-07-11 14:57:46 -0700

    draft: GIF render functionality

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 PYTHON3 := $(shell command -v python3)
 
-jhu: data
-	find data -name '*.png' -print0 | xargs -0 open
-.PHONY: jhu
+jhu.gif: data
+	@#find data -name '*.png' -print0 | xargs -0 open
+	@open "$@"
 
 data:
-	"$(PYTHON3)" -c 'import requests' \
-		|| "$(PYTHON)" -m pip install --user requests
-	"$(PYTHON3)" jhu.py
+	"$(PYTHON3)" -c 'import imageio, requests' \
+		|| "$(PYTHON)" -m pip install --user imageio requests
+	"$(PYTHON3)" jhu.py gif data jhu.gif
--- jhu.py
+++ jhu.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python3
-import os, shutil
-import requests
+import os, shutil, sys
+import imageio, requests
 
-prefix = 'https://storage.googleapis.com/crc-assets/state-timeline/images'
-images = prefix + '/downloads/state-timeline-%s-%s-event-0.png'
-
-states = (
+STATES = (
 'alabama',
 'alaska',
 'arizona',
@@ -58,19 +55,43 @@ states = (
 'wyoming',
 )
 
-def main():
+USAGES = 'USAGE: %s gif data_dir out_gif'
+PREFIX = 'https://storage.googleapis.com/crc-assets/state-timeline/images'
+IMAGES = PREFIX + '/downloads/state-timeline-%s-%s-event-0.png'
+
+def data():
     '''Save 100x images into two data folders'''
     data_path = '%s/%s' % (os.getcwd(), 'data')
     for variant in ('new-confirmed-cases', 'new-deaths'):
         variant_path = '%s/%s' % (data_path, variant)
         os.makedirs(variant_path, exist_ok=True)
-        for state in states:
+        for state in STATES:
             file_path = '%s/%s-%s.png' % (variant_path, state, variant)
-            r = requests.get(images % (variant, state), stream=True)
+            r = requests.get(IMAGES % (variant, state), stream=True)
             with open(file_path, 'wb') as f:
                 r.raw.decode_content = True
                 shutil.copyfileobj(r.raw, f)
             print(file_path)
 
+def gifs(data_path='data', gif_path='jhu.gif'):
+    '''Make three .gif files: one in each of two variants + one combined'''
+    print('-- generating GIFs...')
+    gif_path_home = os.getcwd() # TODO: pass this?
+    #gif_path_doom = '%s/new-death.%s' % (gif_path_home, gif_path)
+    #gif_path_case = '%s/new-confirmed-cases.%s' % (gif_path_home, gif_path)
+    gif_path_both = '%s/%s' % (gif_path_home, gif_path)
+    with imageio.get_writer(gif_path_both, mode='I') as writer:
+        for variant in ('new-confirmed-cases', 'new-deaths'):
+            variant_path = '%s/%s' % (data_path, variant)
+            for state in STATES:
+                file_path = '%s/%s-%s.png' % (variant_path, state, variant)
+                writer.append_data(imageio.imread(file_path))
+
 if __name__ == '__main__':
-    main()
+    '''USAGE: no args = download JHU images; "$0 gif" turns data into GIFs'''
+    program_name = sys.argv[0]
+    has_args = len(sys.argv) > 1
+    build_gifs = (sys.argv[1] == 'gif') if len(sys.argv) == 4 else False
+    if build_gifs or not has_args: data()
+    if build_gifs: gifs(data_path=sys.argv[2], gif_path=sys.argv[3])
+    elif has_args: print(USAGES % (program_name))
