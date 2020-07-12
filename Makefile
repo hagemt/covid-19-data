2af8837dc637da1b8cbbb02c40b49d1e426ea10c
Tor Hagemann <tor.hagemann@socotra.com>
2020-07-12 12:07:16 -0700

    jhy.py: GIF and Makefile target fixup

--- Makefile
+++ Makefile
@@ -1,10 +1,21 @@
+BROWSER := /Applications/Google Chrome.app
 PYTHON3 := $(shell command -v python3)
 
-jhu.gif: data
-	@#find data -name '*.png' -print0 | xargs -0 open
-	@open "$@"
+default: jhu.gif
+	# run w/ BROWSER= path to application that opens GIFs
+	@open -a "$(BROWSER)" --args "file://$(shell pwd)/$<"
+.PHONY: default
 
-data:
+data: sane
+	"$(PYTHON3)" jhu.py data
+	#find data -name '*.png' -print0 | xargs -0 open
+
+sane:
+	[[ -x "$(PYTHON3)" ]] # run w/ PYTHON3= path to python v3.2+
 	"$(PYTHON3)" -c 'import imageio, requests' \
-		|| "$(PYTHON)" -m pip install --user imageio requests
-	"$(PYTHON3)" jhu.py gif data jhu.gif
+		|| "$(PYTHON3)" -m pip install --user imageio requests
+.PHONY: sane
+
+jhu.gif: data
+	#find data -name '*.png' -print0 | xargs -0 open
+	"$(PYTHON3)" jhu.py data "$@"
--- jhu.py
+++ jhu.py
@@ -55,13 +55,13 @@ STATES = (
 'wyoming',
 )
 
-USAGES = 'USAGE: %s gif data_dir out_gif'
+USAGES = 'USAGE: %s data <out_gif>'
 PREFIX = 'https://storage.googleapis.com/crc-assets/state-timeline/images'
 IMAGES = PREFIX + '/downloads/state-timeline-%s-%s-event-0.png'
 
-def data():
+def data(data_dir='data', root_path='.'):
     '''Save 100x images into two data folders'''
-    data_path = '%s/%s' % (os.getcwd(), 'data')
+    data_path = '%s/%s' % (root_path, data_dir)
     for variant in ('new-confirmed-cases', 'new-deaths'):
         variant_path = '%s/%s' % (data_path, variant)
         os.makedirs(variant_path, exist_ok=True)
@@ -73,25 +73,33 @@ def data():
                 shutil.copyfileobj(r.raw, f)
             print(file_path)
 
-def gifs(data_path='data', gif_path='jhu.gif'):
+def gifs(data_dir='data', root_path='.', out_gif='out.gif'):
     '''Make three .gif files: one in each of two variants + one combined'''
-    print('-- generating GIFs...')
-    gif_path_home = os.getcwd() # TODO: pass this?
-    #gif_path_doom = '%s/new-death.%s' % (gif_path_home, gif_path)
-    #gif_path_case = '%s/new-confirmed-cases.%s' % (gif_path_home, gif_path)
-    gif_path_both = '%s/%s' % (gif_path_home, gif_path)
-    with imageio.get_writer(gif_path_both, mode='I') as writer:
+    print('-- generating JHU GIF(s)...')
+    gif_path = '%s/%s' % (root_path, out_gif)
+    with imageio.get_writer(gif_path, mode='I') as gif:
+        print(gif_path)
+        data_path = '%s/%s' % (root_path, data_dir)
         for variant in ('new-confirmed-cases', 'new-deaths'):
             variant_path = '%s/%s' % (data_path, variant)
             for state in STATES:
                 file_path = '%s/%s-%s.png' % (variant_path, state, variant)
-                writer.append_data(imageio.imread(file_path))
+                gif.append_data(imageio.imread(file_path))
+                print(file_path)
 
 if __name__ == '__main__':
     '''USAGE: no args = download JHU images; "$0 gif" turns data into GIFs'''
-    program_name = sys.argv[0]
-    has_args = len(sys.argv) > 1
-    build_gifs = (sys.argv[1] == 'gif') if len(sys.argv) == 4 else False
-    if build_gifs or not has_args: data()
-    if build_gifs: gifs(data_path=sys.argv[2], gif_path=sys.argv[3])
-    elif has_args: print(USAGES % (program_name))
+    out_gif = None if len(sys.argv) < 3 else sys.argv[2] # optional
+    data_dir = 'data' if len(sys.argv) < 2 else sys.argv[1]
+    root_path = os.getcwd() # FIXME: build paths properly
+    error_bad = False # XXX: detect valid images, etc.
+    warn_bad = False # TODO: detect root overwrite
+    if error_bad or warn_bad:
+        print(USAGES % (sys.argv[0]))
+        if warn_bad: print('ERROR: %s (bad directory) or %s (data overwrite)'
+                           % (data_path, file_out))
+        sys.exit(1)
+    if not os.path.exists(data_dir):
+        data(data_dir=data_dir, root_path=root_path)
+    if out_gif:
+        gifs(data_dir=data_dir, root_path=root_path, out_gif=out_gif)
